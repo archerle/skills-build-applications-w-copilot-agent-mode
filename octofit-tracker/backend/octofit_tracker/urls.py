ad9d7e0bcf7689ea93b444016e9df90d3cdea7d4
archerle <93228742+archerle@users.noreply.github.com>
2025-12-17 17:13:19 +0000

    Actualizar api_root para construir URLs absolutas utilizando el nombre del codespace.

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -20,6 +20,7 @@ from .views import TeamViewSet, UserViewSet, ActivityViewSet, WorkoutViewSet, Le
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+import os
 
 router = routers.DefaultRouter()
 router.register(r'teams', TeamViewSet)
@@ -30,12 +31,17 @@ router.register(r'leaderboard', LeaderboardViewSet)
 
 @api_view(['GET'])
 def api_root(request, format=None):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f"https://{codespace_name}-8000.app.github.dev"
+    else:
+        base_url = request.build_absolute_uri('/')[:-1]
     return Response({
-        'teams': reverse('team-list', request=request, format=format),
-        'users': reverse('user-list', request=request, format=format),
-        'activities': reverse('activity-list', request=request, format=format),
-        'workouts': reverse('workout-list', request=request, format=format),
-        'leaderboard': reverse('leaderboard-list', request=request, format=format),
+        'teams': base_url + reverse('team-list'),
+        'users': base_url + reverse('user-list'),
+        'activities': base_url + reverse('activity-list'),
+        'workouts': base_url + reverse('workout-list'),
+        'leaderboard': base_url + reverse('leaderboard-list'),
     })
 
 urlpatterns = [
